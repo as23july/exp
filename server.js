3c1e8e64a42fc3c27b7649417f7f3b44cf5abd78
Aditya <as23july@gmail.com>
2023-04-07 19:02:28 +0530

    hhh

--- server.js
+++ server.js
@@ -5,9 +5,5 @@ const port = 5000;
 
 
 app.listen(port, () => {
-<<<<<<< HEAD
-    console.log(`server runnghing ggg ondffddhkfhdkj dkjshdkjshkdjh port dfgdfg ghfgh fghfghkjdfgh ghh ${port}`); 
-=======
     console.log(`server port ${port}`); 
->>>>>>> new--feature
 });
